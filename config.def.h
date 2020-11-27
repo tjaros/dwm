80f60ce57b0bd6699e4fdf478f8f47b24a2623b0
mewthi <tomisawsm@gmail.com>
2020-11-27 22:25:17 +0100

    changed colors to a bit nord-colorscheme like way

--- config.def.h
+++ config.def.h
@@ -13,15 +13,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_bg1[] = "#3b4252";
+static const char col_bg[] =  "#4c566a";
+static const char col_fg[] =  "#eceff4";
+static const char col_fg1[] = "#81a1c1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg,    col_bg1,    col_bg1 },
+	[SchemeSel]  = { col_fg1,   col_bg,     col_bg },
 };
 
 /* tagging */
@@ -88,7 +87,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run",
+                                  "-m", dmenumon, 
+				  "-fn", dmenufont, 
+				  "-nb", col_bg1, 
+				  "-nf", col_fg, 
+				  "-sb", col_bg, 
+				  "-sf", col_fg1,
+				  NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
