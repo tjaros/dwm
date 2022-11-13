5d0f7ae89837d776d957bc6af6be7d866f14f454
Tomáš Jaroš <xjaros2@fi.muni.cz>
2022-11-13 11:52:41 +0100

    sth sth

--- config.h
+++ config.h
@@ -1,4 +1,3 @@
-
 /* See LICENSE file for copyright and license details. */
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
@@ -11,13 +10,19 @@ static const unsigned int gappov    = 15;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mononoki Nerd Font:pixelsize=14:antialias=true:autohint=true" };
+//static const char *fonts[]          = { "mononoki Nerd Font:pixelsize=14:antialias=true:autohint=true" };
+static const char *fonts[]          = { "PragmataPro Mono Liga:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_bg1[] = "#2e3440"; //3b4252
-static const char col_bg[] =  "#4c566a";
-static const char border1[] =  "#3b4252";
-static const char col_fg[] =  "#eceff4";
-static const char col_fg1[] = "#81a1c1";
+//static const char col_bg1[] = "#2e3440"; //3b4252
+//static const char col_bg[] =  "#4c566a";
+//static const char border1[] =  "#3b4252";
+//static const char col_fg[] =  "#eceff4";
+//static const char col_fg1[] = "#81a1c1";
+static const char col_bg1[] = "#000000";
+static const char col_bg[] =  "#14171b";
+static const char border1[] =  "#cdcdcd";
+static const char col_fg[] =  "#cdcdcd";
+static const char col_fg1[] = "#7d8fa3";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_fg,    col_bg1,   border1   },
@@ -79,9 +84,9 @@ static const Layout layouts[] = {
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
 	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
-	{ MOD, XK_v,     ACTION##stack, {.i = 0 } }, 
+	{ MOD, XK_v,     ACTION##stack, {.i = 0 } },
 /*	{ MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \ */
-/*	{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \	     */
+/*	{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \		 */
 /*	{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \       */
 /*	{ MOD, XK_x,     ACTION##stack, {.i = -1 } },        */
 
@@ -91,22 +96,24 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /*static const char *dmenucmd[] = { "dmenu_run",
-                                  "-m", dmenumon, 
-				  "-fn", dmenufont, 
-				  "-nb", col_bg1, 
-				  "-nf", col_fg, 
-				  "-sb", col_bg, 
+								  "-m", dmenumon,
+				  "-fn", dmenufont,
+				  "-nb", col_bg1,
+				  "-nf", col_fg,
+				  "-sb", col_bg,
 				  "-sf", col_fg1,
 				  NULL };*/
 static const char *rofi_druncmd[] = { "rofi", "-show", "drun", NULL };
 static const char *rofi_runcmd[]  = { "rofi", "-show", "run",  NULL };
 static const char *termcmd[]      =  { "st", NULL };
+static const char *editorcmd[]      =  { "emacs", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,        spawn,          {.v = rofi_druncmd } },
-        { MODKEY|ShiftMask,             XK_space,        spawn,          {.v = rofi_runcmd } },
+		{ MODKEY|ShiftMask,             XK_space,        spawn,          {.v = rofi_runcmd } },
 	{ MODKEY,                       XK_Return,       spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_e,            spawn,          {.v = editorcmd } },
 	{ MODKEY,                       XK_i,            togglebar,      {0} },
 	STACKKEYS(MODKEY,                                focus)
 	STACKKEYS(MODKEY|ShiftMask,                      push)
@@ -114,7 +121,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_o,        incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,            setmfact,       {.f = +0.05} },
-      //{ MODKEY,                       XK_Return,       zoom,           {0} },     the hell is this?
+	  //{ MODKEY,                       XK_Return,       zoom,           {0} },     the hell is this?
 	{ MODKEY,                       XK_Down,   moveresize,     {.v = "0x 25y 0w 0h" } },
 	{ MODKEY,                       XK_Up,     moveresize,     {.v = "0x -25y 0w 0h" } },
 	{ MODKEY,                       XK_Right,  moveresize,     {.v = "25x 0y 0w 0h" } },
@@ -147,10 +154,10 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,            incrovgaps,     {.i = -1 } },
 	{ MODKEY|Mod4Mask,              XK_0,            togglegaps,     {0} },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,            defaultgaps,    {0} },
-    	{ MODKEY,                       XK_Tab,          view,           {0} }, 
+		{ MODKEY,                       XK_Tab,          view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,            killclient,     {0} },
 	{ MODKEY,                       XK_t,            setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,        		XK_BackSpace,    cyclelayout,    {.i = -1 } },
+	{ MODKEY,				XK_BackSpace,    cyclelayout,    {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_BackSpace,    cyclelayout,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_f,            togglefloating, {0} },
 	{ MODKEY,                       XK_s,            togglesticky,   {0} },
@@ -173,11 +180,15 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,            quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_t,        spawn,         SHCMD("touchpad_toggle")},
+	{0, XF86XK_TouchpadOn,    spawn, SHCMD("touchpad_toggle")},
+	{0, XF86XK_TouchpadOff,    spawn, SHCMD("touchpad_toggle")},
+	{0, XF86XK_TouchpadToggle,    spawn, SHCMD("touchpad_toggle")},
 	{0, XF86XK_AudioRaiseVolume,    spawn, SHCMD("pamixer -i 5")},
 	{0, XF86XK_AudioLowerVolume,    spawn, SHCMD("pamixer -d 5")},
 	{0, XF86XK_MonBrightnessUp,     spawn, SHCMD("xbacklight -inc 5")},
 	{0, XF86XK_MonBrightnessDown,   spawn, SHCMD("xbacklight -dec 5")},
-	{0,	    XK_Print,           spawn, SHCMD("SCR=~/pictures/screenshots/$(date +'%d-%m-%Y-%T').png;maim > $SCR && cat $SCR | xclip -selection clipboard -t image/png -i")},
+	{0,		XK_Print,           spawn, SHCMD("SCR=~/pictures/screenshots/$(date +'%d-%m-%Y-%T').png;maim > $SCR && cat $SCR | xclip -selection clipboard -t image/png -i")},
 	{ShiftMask, XK_Print,           spawn, SHCMD("SCR=~/pictures/screenshots/$(date +'%d-%m-%Y-%T').png;maim -s > $SCR && cat $SCR | xclip -selection clipboard -t image/png -i")},
 };
 
@@ -197,4 +208,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
