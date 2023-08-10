76009c74f8ffc7d6167d8dc31925b53ea67c420a
Tomas Jaros <tjaros.822@gmail.com>
2023-08-10 13:47:10 +0200

    fix: change font size and spackng of bar

--- config.def.h
+++ config.def.h
@@ -12,11 +12,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 0;   /* systray spacing */
+static const unsigned int systrayspacing = 1;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 //static const char *fonts[]          = { "mononoki Nerd Font:pixelsize=14:antialias=true:autohint=true" };
-static const char *fonts[]          = { "Hack:style=Regular:pixelsize=14:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Iosevka NFM:style=Regular:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 //static const char col_bg1[] = "#2e3440"; //3b4252
 //static const char col_bg[] =  "#4c566a";
